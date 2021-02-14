4486f92cc6a7fffd805ae957e52b96ff6b1fa952
IRRT <zindgg@gmail.com>
2021-02-14 11:03:11 +0300

    project JDBC PostgreSQL(create table,insert into)

--- src/main/java/com/irrt/springdatajpa/SpringdatajpaApplication.java
+++ src/main/java/com/irrt/springdatajpa/SpringdatajpaApplication.java
@@ -1,31 +1,35 @@
 package com.irrt.springdatajpa;
 import com.irrt.springdatajpa.entity.Person;
 import com.irrt.springdatajpa.jdbc.PersonJdbcDao;
+import com.irrt.springdatajpa.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class SpringdatajpaApplication implements CommandLineRunner{
+public class SpringdataJpaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringdatajpaApplication.class, args);
+		SpringApplication.run(SpringdataJpaApplication.class, args);
 	}
+
 	@Autowired
-	PersonJdbcDao personJdbcDao;
+	PersonRepository personRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		Person anna = new Person(100, "Anna", "Andreeva", "Samara");
-		Person olesya = new Person(101, "Olesya", "Andreeva", "Moscow");
-		Person alex = new Person(102, "Alex", "Andreev", "Omsk");
-
-		personJdbcDao.insert(anna);
-		personJdbcDao.insert(olesya);
-		personJdbcDao.insert(alex);
-
-		System.out.println("<<<<<<<<"+personJdbcDao.findAll());
+//		Person anna = new Person("Anna", "Andreeva", "Samara");
+//		Person olesya = new Person("Olesya", "Andreeva", "Moscow");
+//		Person alex = new Person("Alex", "Andreev", "Omsk");
+//
+//		List<Person> people = Arrays.asList(anna, olesya, alex);
+//        personRepository.saveAll(people);
+//
+//	System.out.println("<<<<<<<<"+personRepository.findAll());
 	}
 }
--- src/main/java/com/irrt/springdatajpa/entity/Person.java
+++ src/main/java/com/irrt/springdatajpa/entity/Person.java
@@ -1,12 +1,26 @@
 package com.irrt.springdatajpa.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
      private int id;
+
      private String firstName;
      private String lastName;
      private String address;
 
     public Person() {
+    }
+
+    public Person(String firstName, String lastName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
     }
 
     public Person(int id, String firstName, String lastName, String address) {
